82d5e0bb0f6bf7d3fe58af2ba2aede3d01659044
Vitor Sousa <vitor@Vitors-MacBook-Pro.local>
2024-08-25 11:56:04 +1000

    Updated action to create issue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const github = require('@actions/github')
 
 /**
  * The main function for the action.
@@ -7,19 +7,31 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    // Get action inputs
+    const token = core.getInput('token', { required: true })
+    const title = core.getInput('title', { required: true })
+    const body = core.getInput('body', { required: false })
+    const assignees = core.getInput('assignees', { required: false })
+
+    // Create a new GitHub client
+    const octokit = github.getOctokit(token)
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`Making request to create issue ...`)
+    const response = await octokit.rest.issues.create({
+      // owner: github.context.repo.owner,
+      // repo: github.context.repo.repo,
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split('\n') : undefined
+    })
+    core.debug(`Successfully created issue..`)
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('issue', JSON.stringify(response.data))
   } catch (error) {
+    core.error(`Failed to create issue..`)
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
   }
